e49e16fe3612676e7005a98cbfbecb52c0a73215
xyx_yang@foxmail.com <xyx_yang@foxmail.com>
2018-03-25 22:42:25 +0800

    0325

--- src/com/servlet/ThematicPicServlet.java
+++ src/com/servlet/ThematicPicServlet.java
@@ -1,7 +1,28 @@
 package com.servlet;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 public class ThematicPicServlet extends HttpServlet {
+	public ThematicPicServlet() {
+		super();
+	}
+	
+	public void destroy() {
+		super.destroy();
+	}
 
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		super.doGet(req, resp);
+	}
+	
+	
 }
